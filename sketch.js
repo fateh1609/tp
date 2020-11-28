777927d2dc472649a59d3b50566477c5d033d552
fateh <71831837+fateh1609@users.noreply.github.com>
2020-11-28 12:43:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground; 
+var stand1,stand2;
+var slingShot;
+var polygon_img;
+
+function preload(){
+polygon_img=loadImage("polygon.png"); }
+
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine)
+  ground = new Ground(400, 400, 100, 1000);
+  //stand1 = new Stand(390,300,250,10);
+  //stand2 = new Stand(700,200,200,10);
+  block1 = new Block(300,275,30,40);
+  console.log(block1);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+ block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+   block15 = new Block(420,195,30,40);
+block16 = new Block(390,155,30,40);
+ blocks1 = new Block(640,175,30,40);
+  blocks2 = new Block(670,175,30,40); 
+  blocks3 = new Block(700,175,30,40);
+ blocks4 = new Block(730,175,30,40);
+ blocks5 = new Block(760,175,30,40);
+   blocks6 = new Block(670,135,30,40);
+ blocks7 = new Block(700,135,30,40);
+ blocks8 = new Block(730,135,30,40);
+  blocks9 = new Block(700,95,30,40);
+  ball = Bodies.circle(50,200,20);
+   World.add(world,ball);
+ slingShot = new Slingshot(this.ball,{x:100,y:200})
+}
+
+function draw() {
+  background(56,44,44);
+  //Engine.update(engine);
+ // text(mouseX + ',' + mouseY, 10, 15);
+  textSize(20); fill("lightyellow");
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
+  ground.display();
+  stand1.display();
+  stand2.display();
+        
+
+}
+
